c45212cab7ff70feef08fc2ff471edb6d98ab843
Matthew Southcott <mjsouthcott@gmail.com>
2020-07-04 23:52:35 -0400

    Fix style syntax in VehicleAvatar component

--- src/components/VehicleAvatar/index.js
+++ src/components/VehicleAvatar/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import { Avatar } from '@material-ui/core'
 
 function VehicleAvatar (props) {
-  const avatarStyle = {
-    backgroundColor: props.backgroundColor,
-    height: 100,
-    width: 100
-  }
-  const imageStyle = {
-    width: props.imageWidth
-  }
+  const useStyles = makeStyles({
+    avatar: {
+      backgroundColor: props.backgroundColor,
+      height: 100,
+      width: 100
+    },
+    img: {
+      width: props.imageWidth
+    }
+  })
+  const classes = useStyles()
 
   return (
-    <Avatar  style={avatarStyle}>
-      <img src={props.icon} alt={props.type} style={imageStyle}/>
+    <Avatar className={classes.avatar}>
+      <img className={classes.img} src={props.icon} alt={props.type} />
     </Avatar>
   )
 }
