251cf19cfd667b9f2eecc827a15751e5e6c1f49a
Max <maxguojiaqi@gmail.com>
2020-07-14 21:38:40 -0400

    add icons and add all the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,32 @@
+// bring in all the react related 
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// bring in all the pages 
+
 import CreateOperator from './pages/CreateOperator'
 import CreateOperatorVehicle from './pages/CreateOperatorVehicle'
 import CreateTechnician from './pages/CreateTechnician'
 import CreateTechnicianVehicle from './pages/CreateTechnicianVehicle'
-import CreateRepairRequestForm from './components/CreateRepairRequestForm/index'
-import CreateRepairWorkOrderForm from './components/CreateRepairWorkOrderForm/index'
+// import CreateRepairRequest from './pages/CreateRepairRequest'
+// import CreateRepairWorkOrder from './pages/CreateRepairWorkOrder'
+import DisplayOperators from './pages/DisplayOperators'
+import DisplayOperatorVehicles from './pages/DisplayOperatorVehicles'
+import DisplayTechnicians from './pages/DisplayTechnicians'
+import DisplayTechnicianVehicles from './pages/DisplayTechnicianVehicles'
+// bring in all the components
+import LayoutCanvas from './components/LayoutCanvas';
 import Dashboard from './components/Dashboard'
+// bring in all the global css
 import './App.css';
-<<<<<<< HEAD
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-=======
-import { BrowserRouter as Router, Route } from "react-router-dom";
->>>>>>> master
-import Navigation from './components/Navigation';
+// bring in all the contexts
 import UserContext from './utils/UserContext'
 
-<<<<<<< HEAD
 
 function App() {
 
   // TODO: the default state value is suppose be grapped from useEffect 
   const [userState, setUserState] = useState({
-    isManager : false,
+    isManager : true,
     role: 'Operations Manager',
     rank: '',
     firstName: '',
@@ -36,45 +40,44 @@ function App() {
 
 
 // // TODO: Logic of Passport JS goes in here
-//   useEffect(() => {
-//     setUserState({
-//       role: 'Operations Manager',
-//       rank: 'Captain',
-//       firstName: 'Max',
-//       lastName: 'Guo',
-//       occupation: 'Armoured Officer',
-//       available: false,
-//       mounted: true,
-//       username: 'maxguo',
-//       password: 'password',
-//     })
-//   }, []); 
-const unauthorized = <div>You are not allowed to view this buddy! </div>
-=======
-function App () {
->>>>>>> master
+//   useEffect(() => {setUserState()}, []); 
+
+  const unauthorized = <div>You are not allowed to view this buddy! </div>
+
   return (
       <Router>
         <UserContext.Provider value={userState}>
-        <Navigation>
+        <LayoutCanvas>
         <Switch>
-          <Route path="/repair-request" component={CreateRepairRequestForm} />
-          <Route path="/repair-workorder" component={CreateRepairWorkOrderForm} />
-          <Route path="/create-operator" >
+          {/* conditional render all the page if not authorized to see */}
+
+          <Route path="/display-operator" component={DisplayOperators} />
+          <Route path="/display-operatorVehicles" component={DisplayOperatorVehicles} />
+          <Route path="/display-technicians" component={DisplayTechnicians} />
+          <Route path="/display-technicianVehicles" component={DisplayTechnicianVehicles} />
+          <Route path="/repair-request"/>
+          <Route path="/repair-workorder"/>
+
+
+          <Route path="/create-operator">
             {userState.isManager ? <CreateOperator/> : unauthorized}
           </Route>
-          <Route path="/create-technician" component={CreateTechnician} />
-          <Route path="/create-operatorVehicle" component={CreateOperatorVehicle} />
-          <Route path="/create-technicianVehicle" component={CreateTechnicianVehicle} />
-          <Route path="/" component={Dashboard} />
+          <Route path="/create-technician">
+          {userState.isManager ? <CreateTechnician/> : unauthorized}
+          </Route>
+          <Route path="/create-operatorVehicle">
+          {userState.isManager ? <CreateOperatorVehicle/> : unauthorized}
+          </Route>
+          <Route path="/create-technicianVehicle">
+          {userState.isManager ? <CreateTechnicianVehicle/> : unauthorized}
+          </Route>
+          <Route path="/" >
+          {userState.isManager ? <Dashboard/> : unauthorized}
+          </Route>
         </Switch> 
-        </Navigation>
+        </LayoutCanvas>
         </UserContext.Provider>
       </Router>
-<<<<<<< HEAD
-=======
-    </>
->>>>>>> master
   )
 }
 
