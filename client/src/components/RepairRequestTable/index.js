91850e12b6c6fe7662b505e1c3a8b1d624cdeb71
Matthew Southcott <mjsouthcott@gmail.com>
2020-07-16 14:25:53 -0400

    Update DisplayRepairRequests page

--- client/src/components/RepairRequestTable/index.js
+++ client/src/components/RepairRequestTable/index.js
@@ -46,8 +46,8 @@ function RepairRequestTable(props) {
                   <TableCell align="right">
                     Crew Remained With Vehicle
                   </TableCell>
-                  <TableCell align="right">Latitude</TableCell>
-                  <TableCell align="right">Longitude</TableCell>
+                  {/* <TableCell align="right">Latitude</TableCell>
+                  <TableCell align="right">Longitude</TableCell> */}
                   <TableCell align="right">Status</TableCell>
                   <TableCell align="right">Technician</TableCell>
                   <TableCell align="right">Repair Work Order</TableCell>
@@ -57,36 +57,60 @@ function RepairRequestTable(props) {
                 {props.vehicles.map((vehicle) => (
                   <TableRow key={vehicle._id}>
                     <TableCell component="th" scope="row">
-                      {vehicle.repairRequests[0].number}
+                      {vehicle.repairRequests[0]
+                        ? vehicle.repairRequests[0].number
+                        : null}
                     </TableCell>
                     <TableCell align="right">{vehicle.callSign}</TableCell>
                     <TableCell align="right">{vehicle.type}</TableCell>
                     <TableCell align="right">
-                      {vehicle.repairRequest[0].estimatedConditionClass}
+                      {vehicle.repairRequests[0]
+                        ? vehicle.repairRequests[0].estimatedConditionClass
+                        : null}
                     </TableCell>
                     <TableCell align="right">
-                      {vehicle.repairRequest[0].vehicleCanBeMovedBy}
+                      {vehicle.repairRequests[0]
+                        ? vehicle.repairRequests[0].vehicleCanBeMovedBy
+                        : null}
                     </TableCell>
                     <TableCell align="right">
-                      {vehicle.repairRequest[0].localTacticalSituation}
+                      {vehicle.repairRequests[0]
+                        ? vehicle.repairRequests[0].localTacticalSituation
+                        : null}
                     </TableCell>
                     <TableCell align="right">
-                      {vehicle.repairRequest[0].crewRemainedWithVehicle}
+                      {vehicle.repairRequests[0] &&
+                      vehicle.repairRequests[0].crewRemainedWithVehicle
+                        ? "Yes"
+                        : "No"}
+                    </TableCell>
+                    {/* <TableCell align="right">
+                      {vehicle.repairRequests[0]
+                        ? vehicle.repairRequests[0].location.latitude
+                        : null}
                     </TableCell>
                     <TableCell align="right">
-                      {vehicle.repairRequest[0].latitude}
+                      {vehicle.repairRequests[0]
+                        ? vehicle.repairRequests[0].location.longitude
+                        : null}
+                    </TableCell> */}
+                    <TableCell align="right">
+                      {vehicle.repairRequests[0]
+                        ? vehicle.repairRequests[0].status
+                        : null}
                     </TableCell>
                     <TableCell align="right">
-                      {vehicle.repairRequest[0].longitude}
+                      {vehicle.repairRequests[0] &&
+                      vehicle.repairRequests[0].assignedTo
+                        ? "Assigned"
+                        : // ? `${vehicle.repairRequests[0].assignedTo.rank} ${vehicle.repairRequests[0].assignedTo.firstName} ${vehicle.repairRequests[0].assignedTo.lastName}`
+                          "Not Assigned"}
                     </TableCell>
                     <TableCell align="right">
-                      {vehicle.repairRequest[0].status}
-                    </TableCell>
-                    <TableCell align="right">
-                      {vehicle.repairRequest[0].assignedTo}
-                    </TableCell>
-                    <TableCell align="right">
-                      {vehicle.repairRequest[0].repairWorkOrder[0]._id}
+                      {vehicle.repairRequests[0] &&
+                      vehicle.repairRequests[0].repairWorkOrder
+                        ? vehicle.repairRequests[0].repairWorkOrder._id
+                        : null}
                     </TableCell>
                   </TableRow>
                 ))}
