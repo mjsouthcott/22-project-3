644614799bbace1d93e0319e4e5b25e5b9dc66cf
Matthew Southcott <mjsouthcott@gmail.com>
2020-07-18 14:17:04 -0400

    Add technician view and display message indicating no repair requests when applicable

--- client/src/components/RepairRequestTable/index.js
+++ client/src/components/RepairRequestTable/index.js
@@ -35,142 +35,175 @@ function RepairRequestTable(props) {
           <Typography variant="h4" className={classes.typography}>
             Display Repair Requests
           </Typography>
-          <TableContainer>
-            <Table className={classes.table} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Number</TableCell>
-                  <TableCell align="right">Vehicle Call Sign</TableCell>
-                  <TableCell align="right">Vehicle Type</TableCell>
-                  <TableCell align="right">Estimated Condition Class</TableCell>
-                  <TableCell align="right">Vehicle Can Be Moved By</TableCell>
-                  <TableCell align="right">Local Tactical Situation</TableCell>
-                  <TableCell align="right">
-                    Crew Remained With Vehicle
-                  </TableCell>
-                  <TableCell align="right">Status</TableCell>
-                  <TableCell align="right">Assigned To</TableCell>
-                  <TableCell align="right">Repair Work Order</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {props.vehicles.map((vehicle) =>
-                  vehicle.repairRequests.map((repairRequest) => (
-                    <TableRow key={repairRequest._id}>
-                      <TableCell component="th" scope="row">
-                        {repairRequest.number}
-                      </TableCell>
-                      <TableCell align="right">{vehicle.callSign}</TableCell>
-                      <TableCell align="right">{vehicle.type}</TableCell>
-                      <TableCell align="right">
-                        {repairRequest.estimatedConditionClass}
-                      </TableCell>
-                      <TableCell align="right">
-                        {repairRequest.vehicleCanBeMovedBy}
-                      </TableCell>
-                      <TableCell align="right">
-                        {repairRequest.localTacticalSituation === "Safe" ? (
-                          <Chip
-                            label="Safe"
-                            style={{ color: "white", backgroundColor: "green" }}
-                          />
-                        ) : (
-                          <Chip
-                            label="Hostile"
-                            style={{ color: "white", backgroundColor: "red" }}
-                          />
-                        )}
-                      </TableCell>
-                      <TableCell align="right">
-                        {repairRequest.crewRemainedWithVehicle ? (
-                          <Chip
-                            label="Yes"
-                            style={{ color: "white", backgroundColor: "green" }}
-                          />
-                        ) : (
-                          <Chip
-                            label="No"
-                            style={{ color: "white", backgroundColor: "red" }}
-                          />
-                        )}
-                      </TableCell>
-                      <TableCell align="right">
-                        {repairRequest.status === "Open" ? (
-                          <Chip
-                            label="Open"
-                            style={{ color: "white", backgroundColor: "red" }}
-                          />
-                        ) : repairRequest.status === "Work In Progress" ? (
-                          <Chip
-                            label="Work In Progress"
-                            style={{ color: "black", backgroundColor: "gold" }}
-                          />
-                        ) : (
-                          <Chip
-                            label="Complete"
-                            style={{ color: "white", backgroundColor: "green" }}
-                          />
-                        )}
-                      </TableCell>
-                      <TableCell align="right">
-                        {repairRequest.assignedTo ? (
-                          `${repairRequest.assignedTo.rank} ${repairRequest.assignedTo.firstName} ${repairRequest.assignedTo.lastName}`
-                        ) : (
-                          <AssignTechnicianInput
-                            repairRequest={repairRequest}
-                            availableTechnicians={props.availableTechnicians}
-                            updateVehicles={props.updateVehicles}
-                            updateAvailableTechnicians={
-                              props.updateAvailableTechnicians
-                            }
-                          />
-                        )}
-                      </TableCell>
-                      <TableCell align="right">
-                        {repairRequest.repairWorkOrder === undefined &&
-                        props.role === "Maintenance Manager" ? (
-                          <Chip
-                            label="Not Created"
-                            style={{ color: "white", backgroundColor: "red" }}
-                          />
-                        ) : repairRequest.repairWorkOrder === undefined &&
-                          props.role === "Technician" ? (
-                          <Button
-                            variant="contained"
-                            // style={{ color: "white", backgroundColor: "green" }}
-                            onClick={() => {
-                              console.log(
-                                repairRequest._id,
-                                repairRequest.number
-                              );
-                            }}
-                          >
-                            Create
-                          </Button>
-                        ) : repairRequest.repairWorkOrder.status === "Open" ? (
-                          <Chip
-                            label="Open"
-                            style={{ color: "white", backgroundColor: "red" }}
-                          />
-                        ) : repairRequest.repairWorkOrder.status ===
-                          "Work In Progress" ? (
-                          <Chip
-                            label="Work In Progress"
-                            style={{ color: "black", backgroundColor: "gold" }}
-                          />
-                        ) : (
-                          <Chip
-                            label="Complete"
-                            style={{ color: "white", backgroundColor: "green" }}
-                          />
-                        )}
-                      </TableCell>
-                    </TableRow>
-                  ))
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          {props.vehicles.length === 0 ? (
+            <Typography
+              variant="h6"
+              className={classes.typography}
+              style={{ marginBottom: 0 }}
+            >
+              No Repair Requests to display
+            </Typography>
+          ) : (
+            <TableContainer>
+              <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Number</TableCell>
+                    <TableCell align="right">Vehicle Call Sign</TableCell>
+                    <TableCell align="right">Vehicle Type</TableCell>
+                    <TableCell align="right">
+                      Estimated Condition Class
+                    </TableCell>
+                    <TableCell align="right">Vehicle Can Be Moved By</TableCell>
+                    <TableCell align="right">
+                      Local Tactical Situation
+                    </TableCell>
+                    <TableCell align="right">
+                      Crew Remained With Vehicle
+                    </TableCell>
+                    <TableCell align="right">Status</TableCell>
+                    <TableCell align="right">Assigned To</TableCell>
+                    <TableCell align="right">Repair Work Order</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {props.vehicles.map((vehicle) =>
+                    vehicle.repairRequests.map((repairRequest) => (
+                      <TableRow key={repairRequest._id}>
+                        <TableCell component="th" scope="row">
+                          {repairRequest.number}
+                        </TableCell>
+                        <TableCell align="right">{vehicle.callSign}</TableCell>
+                        <TableCell align="right">{vehicle.type}</TableCell>
+                        <TableCell align="right">
+                          {repairRequest.estimatedConditionClass}
+                        </TableCell>
+                        <TableCell align="right">
+                          {repairRequest.vehicleCanBeMovedBy}
+                        </TableCell>
+                        <TableCell align="right">
+                          {repairRequest.localTacticalSituation === "Safe" ? (
+                            <Chip
+                              label="Safe"
+                              style={{
+                                color: "white",
+                                backgroundColor: "green",
+                              }}
+                            />
+                          ) : (
+                            <Chip
+                              label="Hostile"
+                              style={{ color: "white", backgroundColor: "red" }}
+                            />
+                          )}
+                        </TableCell>
+                        <TableCell align="right">
+                          {repairRequest.crewRemainedWithVehicle ? (
+                            <Chip
+                              label="Yes"
+                              style={{
+                                color: "white",
+                                backgroundColor: "green",
+                              }}
+                            />
+                          ) : (
+                            <Chip
+                              label="No"
+                              style={{ color: "white", backgroundColor: "red" }}
+                            />
+                          )}
+                        </TableCell>
+                        <TableCell align="right">
+                          {repairRequest.status === "Open" ? (
+                            <Chip
+                              label="Open"
+                              style={{ color: "white", backgroundColor: "red" }}
+                            />
+                          ) : repairRequest.status === "Work In Progress" ? (
+                            <Chip
+                              label="Work In Progress"
+                              style={{
+                                color: "black",
+                                backgroundColor: "gold",
+                              }}
+                            />
+                          ) : (
+                            <Chip
+                              label="Complete"
+                              style={{
+                                color: "white",
+                                backgroundColor: "green",
+                              }}
+                            />
+                          )}
+                        </TableCell>
+                        <TableCell align="right">
+                          {repairRequest.assignedTo ? (
+                            `${repairRequest.assignedTo.rank} ${repairRequest.assignedTo.firstName} ${repairRequest.assignedTo.lastName}`
+                          ) : (
+                            <AssignTechnicianInput
+                              repairRequest={repairRequest}
+                              availableTechnicians={props.availableTechnicians}
+                              updateVehicles={props.updateVehicles}
+                              updateAvailableTechnicians={
+                                props.updateAvailableTechnicians
+                              }
+                            />
+                          )}
+                        </TableCell>
+                        <TableCell align="right">
+                          {repairRequest.repairWorkOrder === undefined &&
+                          props.user.role === "Maintenance Manager" ? (
+                            <Chip
+                              label="Not Created"
+                              style={{ color: "white", backgroundColor: "red" }}
+                            />
+                          ) : repairRequest.repairWorkOrder === undefined &&
+                            props.user.role === "Technician" ? (
+                            <Button
+                              variant="contained"
+                              // style={{ color: "white", backgroundColor: "green" }}
+                              onClick={() => {
+                                console.log(
+                                  repairRequest._id,
+                                  repairRequest.number
+                                );
+                              }}
+                            >
+                              Create
+                            </Button>
+                          ) : repairRequest.repairWorkOrder.status ===
+                            "Open" ? (
+                            <Chip
+                              label="Open"
+                              style={{ color: "white", backgroundColor: "red" }}
+                            />
+                          ) : repairRequest.repairWorkOrder.status ===
+                            "Work In Progress" ? (
+                            <Chip
+                              label="Work In Progress"
+                              style={{
+                                color: "black",
+                                backgroundColor: "gold",
+                              }}
+                            />
+                          ) : (
+                            <Chip
+                              label="Complete"
+                              style={{
+                                color: "white",
+                                backgroundColor: "green",
+                              }}
+                            />
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
         </CardContent>
       </Card>
     </Container>
--- client/src/pages/DisplayRepairRequests.js
+++ client/src/pages/DisplayRepairRequests.js
@@ -12,9 +12,23 @@ function DisplayRepairRequests() {
   useEffect(() => {
     API.getVehicles()
       .then((res) => {
-        const filteredVehicles = res.data.filter((vehicle) => {
-          return vehicle.repairRequests.length !== 0;
-        });
+        let filteredVehicles = [];
+        if (currentUser.role === "Maintenance Manager") {
+          filteredVehicles = res.data.filter((vehicle) => {
+            return vehicle.repairRequests.length !== 0;
+          });
+        } else if (currentUser.role === "Technician") {
+          res.data.forEach((vehicle) => {
+            vehicle.repairRequests.forEach((repairRequest) => {
+              if (
+                repairRequest.assignedTo &&
+                repairRequest.assignedTo._id === currentUser._id
+              )
+                filteredVehicles.push(vehicle);
+            });
+          });
+        }
+        console.log(filteredVehicles);
         setVehicles(filteredVehicles);
       })
       .then(() => {
@@ -48,7 +62,7 @@ function DisplayRepairRequests() {
 
   return (
     <RepairRequestTable
-      role={currentUser.role}
+      user={currentUser}
       vehicles={vehicles}
       availableTechnicians={availableTechnicians}
       updateVehicles={updateVehicles}
