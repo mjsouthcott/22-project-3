50e41d90f1fa9cfd3f65b2bbbe8576b6c38edd1c
Matthew Southcott <mjsouthcott@gmail.com>
2020-07-21 08:33:51 -0400

    Prep app for deployment

--- server.js
+++ server.js
@@ -9,16 +9,18 @@ const PORT = process.env.PORT || 3001;
 const passport = require("./client/src/passport/passport.js");
 
 // We need to use sessions to keep track of our user’s login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 //Define middleware here
-
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -28,13 +30,23 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/eTripleRDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true
+//   });
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/eTripleRDB", {
+  process.env.MONGODB_URI ||
+    "mongodb://user:password123@ds023458.mlab.com:23458/heroku_xd6m9tvz",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true
-  });
+    useCreateIndex: true,
+  }
+);
 
 // Start the API server
 app.listen(PORT, function () {
